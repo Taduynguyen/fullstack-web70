2278166cf6ad9493b3e4aaeeea77d76652279998
bsdaoquang <bsdaoquang@gmail.com>
2023-07-19 21:54:57 +0700

    first commit

---
+++ lesson-01/index.js
@@ -0,0 +1,10 @@
+/** @format */
+const express = require('express');
+
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+	console.log(`Server started at: http://localhost:${PORT}`);
+});
