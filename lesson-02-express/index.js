394039ea3658ebefe7e6ea13fe233bfec84777a2
bsdaoquang <bsdaoquang@gmail.com>
2023-07-23 22:06:41 +0700

    add router

--- lesson-02-express/index.js
+++ lesson-02-express/index.js
@@ -1,1 +1,31 @@
 /** @format */
+const express = require('express');
+const authRouter = require('./router/authRouter');
+const productRouter = require('./router/productRouter');
+
+const app = express();
+
+app.use('/auth', authRouter);
+
+// localhost:300/auth/login
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
+
+app.get('/profile/:id/:name', (req, res) => {
+	const id = req.params.id;
+	const name = req.params.name;
+	res.json({ data: `ID: ${id}, name: ${name}` });
+});
+
+app.get('/product', productRouter);
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+	if (!err) {
+		console.log(`Server starting on http://localhost:${PORT}`);
+	} else {
+		console.log(err);
+	}
+});
---
+++ lesson-02-express/router/authRouter.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const { Router } = require('express');
+
+const authRouter = Router();
+
+// authRouter.get('/login', (req, res) => {
+// 	res.json({
+// 		result: 'OK',
+// 	});
+// });
+
+// authRouter.get('/:id', (req, res) => {
+// 	res.json({
+// 		name: '',
+// 		email: '',
+// 	});
+// });
+
+module.exports = authRouter;
